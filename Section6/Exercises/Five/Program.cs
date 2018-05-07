2e5ea56c67f875af7e64c5054f401f500ca59f2d
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-07 18:20:48 -0400

    Finished Section 6

---
+++ Section6/Exercises/Five/Program.cs
@@ -0,0 +1,48 @@
+﻿// Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
+
+using System;
+using System.Collections.Generic;
+
+namespace Five
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a list of comma separated numbers (e.g 5, 1, 9, 2, 10): ");
+            string nums = Console.ReadLine();
+            string[] numArray = nums.Split(",");
+            var numbers = new List <int>();
+            
+
+
+            foreach(var n in numArray)
+            {
+                numbers.Add(Convert.ToInt32(n));
+            }
+
+            if (numbers.Count < 5 || numbers.Count < 1)
+            {
+                Console.WriteLine("Invalid, retry");
+                return;
+            }
+            else
+            {
+                numbers.Sort();
+            }
+
+
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            // foreach(var n in numbers)
+            // {
+            //         System.Console.WriteLine(n);
+            // }
+
+        }
+    }
+}
--- Section6/Exercises/Four/Program.cs
+++ Section6/Exercises/Four/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates. Display the unique numbers that the user has entered.
 
 using System;
+using System.Collections.Generic;
+
 
 namespace Four
 {
@@ -10,22 +12,41 @@ namespace Four
         {
             // Console.WriteLine("Hello World!");
             bool quit = false;
+            var numbers = new List <int>();
+            int number;
+            
 
             while (!quit)
             {
 
                 System.Console.WriteLine("Enter number or enter quit to exit");
                 var input = Console.ReadLine();
-                var number = Convert.ToInt32(Console.ReadLine());
-                if ("quit" == input.ToLower())
+                // var number = Convert.ToInt32(Console.ReadLine());
+                if (input.ToLower() == "quit")
                 {
                     quit = true;
                 }
                 else
                 {
-                    
+                    number = Convert.ToInt32(input);
+                    numbers.Add(number);
                 }
 
+            }
+
+
+            var numbersUnique = new List <int>();
+
+            foreach(var n in numbers)
+            {
+                if (!numbersUnique.Contains(n))
+                {
+                    numbersUnique.Add(n);
+                }
+            }
+            foreach(var n in numbersUnique)
+            {
+                    System.Console.WriteLine(n);
             }
         }
     }
