eb8572850a4104379774c5c8c940ef315d535541
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-09 20:37:52 -0400

    completed Section 10

--- Section10/Notes/Program.cs
+++ Section10/Notes/Program.cs
@@ -12,12 +12,29 @@ namespace Notes
             // check if variables are holding what you expect
             // F9 to insert breakpoint
 
-            // F10 to run one line at a time
+            // F10 to run one line at a time (step over method) 
             // F11 to step into method
-            // Shift+F11 to step put method
+            // Shift+F11 to step out method
+            // Shift + F5 stop debugging
 
             // Watch Window. We can put variables into the window so we can look at them while running
             // red variable means it just changed
+
+
+            // Edge case - uncommon scenarios. Need to plan for these
+
+            // Defensive programming - when we get input, check if its correct before continuing
+
+            // Side effect - not good, make methods unreliable and hard to find bugs in them
+
+            // Defensive programming can prevent your app from going into the wrong 
+state
+            // Call stack shows you the order of methods that were called from when the app was started to where you are now
+
+            // Autos and Locals windows
+            // Autos automatically puts in variables you may be interested in
+            // Locals does not list all variations like auto does. Shows variables in the local scope
+
         }
     }
 }
